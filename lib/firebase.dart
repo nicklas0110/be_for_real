f76cd2f36923ad6a72256ce540b9598cf8351903
AndersJakobsen <ande12n3@easv365.dk>
2023-05-04 11:38:46 +0200

    Firebase file
    Upload image method is made in a new Firebase file

---
+++ lib/firebase.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+
+class Firebase {
+
+ Future<void> uploadImage(File imageFile) async {
+    final fileName = DateTime
+        .now()
+        .millisecondsSinceEpoch
+        .toString();
+    final Reference reference =
+    FirebaseStorage.instance.ref().child('images/$fileName.jpg');
+    final UploadTask uploadTask = reference.putFile(imageFile);
+    final TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() {});
+    final String downloadUrl = await taskSnapshot.ref.getDownloadURL();
+    print('Image URL: $downloadUrl');
+  }
+
+
+}
