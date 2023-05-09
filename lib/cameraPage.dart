645f9dc54389d63fabb250f3929912af6b6e788a
FrederikReigstad <Frederik9876@gmail.com>
2023-05-09 15:08:16 +0200

    Made camera work and take pictures from front and back

--- lib/cameraPage.dart
+++ lib/cameraPage.dart
@@ -1,169 +1,97 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
+import 'chat/screens/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'Camera', friendTab: '', groupTab: '',),
+    );
+  }
+}
 
 // https://docs.flutter.dev/cookbook/plugins/picture-using-camera
 
 class CameraPage extends StatefulWidget {
-  const CameraPage({super.key});
+  const CameraPage({super.key, required this.title});
+  final String title;
 
   @override
   State<CameraPage> createState() => _CameraPageState();
 }
 
 class _CameraPageState extends State<CameraPage> {
-  CameraDescription? camera;
+  CameraController? _controllerBack;
+  CameraController? _controllerFront;
+
+  bool _initialized = false;
 
   @override
   void initState() {
     super.initState();
-    _getCameras().then((cameras) {
+    getFrontAndBackCamera().then((cameras) async {
+      _controllerBack = CameraController(cameras.back, ResolutionPreset.medium);
+      _controllerFront =
+          CameraController(cameras.front, ResolutionPreset.medium);
+      await _controllerBack?.initialize();
+      await _controllerFront?.initialize();
       setState(() {
-        // Set the camera variable to the first back camera
-        camera = cameras.firstWhere(
-              (camera) => camera.lensDirection == CameraLensDirection.back,
-          orElse: () => cameras.first,
-        );
+        _initialized = true;
       });
     });
   }
 
-  Future<List<CameraDescription>> _getCameras() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    final cameras = await availableCameras();
-    return cameras;
-  }
-
   @override
   Widget build(BuildContext context) {
+    if (!_initialized) return Center(child: CircularProgressIndicator());
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        actions: const [
-          Expanded(
-            child: Center(
-              child: Text(
-                  style: TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
-                  "BeForReal"),
-            ),
-          ),
-        ],
+        title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          if (camera == null)
-            _buildCameraSelection()
-          else
-            CameraWidget(
-              camera: camera!,
-              key: ValueKey('camera-${camera!.name}'),
-            )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[CameraPreview(_controllerBack!)],
+        ),
       ),
-    );
-  }
-
-
-  FutureBuilder<List<CameraDescription>> _buildCameraSelection() {
-    return FutureBuilder(
-      future: _getCameras(),
-      builder: (context, snapshot) {
-        return DropdownButton<CameraDescription>(
-            value: camera,
-            hint: const Text('Select a camera'),
-            items: snapshot.data
-                    ?.map((e) => DropdownMenuItem(
-                          value: e,
-                          child: Text('${e.lensDirection.name} ${e.name}'),
-                        ))
-                    .toList() ??
-                [],
-            onChanged: (camera) {
-              setState(() {
-                this.camera = camera;
-              });
-            });
-      },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final file1 = await _controllerBack!.takePicture();
+          final file2 = await _controllerFront!.takePicture();
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Column(children: [
+                Text(file1.path),
+                Text(file2.path),
+              ])));
+        },
+        tooltip: 'Take picture',
+        child: const Icon(Icons.camera),
+      ),
     );
   }
 }
 
-class CameraWidget extends StatefulWidget {
-  final CameraDescription camera;
-  const CameraWidget({required this.camera, super.key});
 
-  @override
-  State<CameraWidget> createState() => _CameraWidgetState();
+
+
+class FrontAndBackCamera {
+  final CameraDescription front;
+  final CameraDescription back;
+
+  FrontAndBackCamera({required this.front, required this.back});
 }
 
-class _CameraWidgetState extends State<CameraWidget> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    // To display the current output from the Camera,
-    // create a CameraController.
-    _controller = CameraController(
-      // Get a specific camera from the list of available cameras.
-      widget.camera,
-      // Define the resolution to use.
-      ResolutionPreset.medium,
-    );
-
-    // Next, initialize the controller. This returns a Future.
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    // Dispose of the controller when the widget is disposed.
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<void>(
-      future: _initializeControllerFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          // If the Future is complete, display the preview.
-          return Column(
-            children: [CameraPreview(_controller), _takePictureButton(context)],
-          );
-        } else {
-          // Otherwise, display a loading indicator.
-          return const Center(child: CircularProgressIndicator());
-        }
-      },
-    );
-  }
-
-  FloatingActionButton _takePictureButton(BuildContext context) {
-    final messenger = ScaffoldMessenger.of(context);
-    return FloatingActionButton(
-      // Provide an onPressed callback.
-      onPressed: () async {
-        // Take the Picture in a try / catch block. If anything goes wrong,
-        // catch the error.
-        try {
-          // Ensure that the camera is initialized.
-          await _initializeControllerFuture;
-
-          // Attempt to take a picture and then get the location
-          // where the image file is saved.
-          final image = await _controller.takePicture();
-          print(image.path);
-          messenger.showSnackBar(SnackBar(content: Text(image.path)));
-        } catch (e) {
-          // If an error occurs, log the error to the console.
-          print(e);
-        }
-      },
-      child: const Icon(Icons.camera_alt),
-    );
-  }
+Future<FrontAndBackCamera> getFrontAndBackCamera() async {
+  final cameras = await availableCameras();
+  return FrontAndBackCamera(
+    front: cameras
+        .firstWhere((cam) => cam.lensDirection == CameraLensDirection.front),
+    back: cameras
+        .firstWhere((cam) => cam.lensDirection == CameraLensDirection.back),
+  );
 }
--- lib/chat/screens/home_page.dart
+++ lib/chat/screens/home_page.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   Route _createRouteCamera() {
     return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => const CameraPage(),
+      pageBuilder: (context, animation, secondaryAnimation) => const CameraPage(title: 'Camera',),
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
         const begin = Offset(0.0, 1.0);
         const end = Offset.zero;
