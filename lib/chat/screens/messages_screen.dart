6dec6afe77a1742b95f978248549fde4956926d4
AndersJakobsen <ande12n3@easv365.dk>
2023-05-23 13:15:38 +0200

    Profile picture in top of the group, and start at the bottum of the message stack

--- lib/chat/screens/messages_screen.dart
+++ lib/chat/screens/messages_screen.dart
@@ -1,46 +1,83 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../chat_service.dart';
-import '../models/user.dart';
-import '../utils.dart';
 import '../models/groups.dart';
 import '../models/message.dart';
+import '../utils.dart';
 import '../widgets/add_member_button.dart';
 
-class MessagesScreen extends StatelessWidget {
-  final padding = 8.0;
+class MessagesScreen extends StatefulWidget {
+  final double padding = 8.0;
   final Groups groups;
   final TextEditingController textEditingController = TextEditingController();
 
-  MessagesScreen({required this.groups, Key? key, required Groups group})
-      : super(key: key);
+  MessagesScreen({
+    required this.groups,
+    Key? key,
+    required Groups group,
+  }) : super(key: key);
+
+  @override
+  _MessagesScreenState createState() => _MessagesScreenState();
+}
+
+class _MessagesScreenState extends State<MessagesScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final chat = Provider.of<ChatService>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text(groups.name),
-        actions: [AddMemberButton(groups: groups)],
+        title: Row(
+          children: [
+            ClipOval(
+              child: Image.network(
+                widget.groups.imageUrl ?? '',
+                width: 40,
+                height: 40,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(width: 8),
+            Text(widget.groups.name),
+          ],
+        ),
+        actions: [
+          AddMemberButton(groups: widget.groups),
+        ],
       ),
       body: Column(
         children: [
           Expanded(
             child: FirestoreListView<Message>(
-              query: chat.messages(groups),
+              query: chat.messages(widget.groups),
+              controller: _scrollController,
+              reverse: true, // Display the messages in reverse order
               itemBuilder: (context, doc) {
                 final message = doc.data();
-                return Column(children: [
-                  sender(message),
-                  textBubble(message),
-                  SizedBox(height: padding * 2),
-                ]);
+                return Column(
+                  children: [
+                    sender(message),
+                    textBubble(message),
+                    SizedBox(height: widget.padding * 2),
+                  ],
+                );
               },
             ),
           ),
-          Align(alignment: Alignment.bottomCenter, child: messageInput(context))
+          Align(alignment: Alignment.bottomCenter, child: messageInput(context)),
         ],
       ),
     );
@@ -51,7 +88,7 @@ class MessagesScreen extends StatelessWidget {
       width: double.infinity,
       child: Card(
         child: Padding(
-          padding: EdgeInsets.all(padding),
+          padding: EdgeInsets.all(widget.padding),
           child: Text(message.content),
         ),
       ),
@@ -60,13 +97,13 @@ class MessagesScreen extends StatelessWidget {
 
   Widget sender(Message message) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: padding),
+      padding: EdgeInsets.symmetric(horizontal: widget.padding),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         mainAxisSize: MainAxisSize.max,
         children: [
           Text(senderName(message.from)),
-          if (message.timestamp != null) Text(since(message.timestamp!))
+          if (message.timestamp != null) Text(since(message.timestamp!)),
         ],
       ),
     );
@@ -76,14 +113,14 @@ class MessagesScreen extends StatelessWidget {
     final user = Provider.of<User>(context);
     final chat = Provider.of<ChatService>(context);
     return Padding(
-      padding: EdgeInsets.all(padding),
+      padding: EdgeInsets.all(widget.padding),
       child: TextField(
-        controller: textEditingController,
+        controller: widget.textEditingController,
         keyboardType: TextInputType.text,
         onSubmitted: (value) {
           if (value.isEmpty) return;
-          chat.sendMessage(user, groups, value);
-          textEditingController.clear();
+          chat.sendMessage(user, widget.groups, value);
+          widget.textEditingController.clear();
         },
         decoration: const InputDecoration(
           border: OutlineInputBorder(),
